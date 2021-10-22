1160316641eac6b6d7a4c6d125bcbf7476405e66
Micheal Limones <micheal@zestylimones.com>
2021-10-22 11:18:10 -0600

    created card css file.

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Card.css';
 
 class Card extends React.Component {
   render() {
     return (
       <div className="Card">
+        <button className="Card-button">delete</button>
         <h3 className="Card-title">this.props.title</h3>
         <p className="Card-content">this.props.content</p>
       </div>
