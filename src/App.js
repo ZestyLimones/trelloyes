d485426f01d86c5989dd897b37887fc9412fa4ea
Micheal Limones <micheal@zestylimones.com>
2021-10-22 12:38:42 -0600

    added list content to app.js file.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,15 @@ class App extends Component() {
         <header className="App-header">
           <h1>Trelloyes!</h1>
         </header>
-        <div className="App-list"></div>
+        <div className="App-list">
+          {store.lists.map((list) => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map((id) => store.allCards[id])}
+            />
+          ))}
+        </div>
       </main>
     );
   }
