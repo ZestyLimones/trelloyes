6a5d068a48574bc302469d61a82a2d34d41315e3
Micheal Limones <micheal@zestylimones.com>
2021-10-22 12:11:44 -0600

    added basic content to app.js file.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import List from './List';
 import './App.css';
 
 function App() {
-  return <main className="App">{/* content goes here */}</main>;
+  return (
+    <main className="App">
+      <header className="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      <div className="App-list"></div>
+    </main>
+  );
 }
 
 export default App;
