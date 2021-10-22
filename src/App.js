b0ec360cab9bf5f902d27b55ebdae73a52d433e6
Micheal Limones <micheal@zestylimones.com>
2021-10-22 12:36:44 -0600

    added more content to app.js file. Added STORE to index.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import List from './List';
 import './App.css';
 
-function App() {
-  return (
-    <main className="App">
-      <header className="App-header">
-        <h1>Trelloyes!</h1>
-      </header>
-      <div className="App-list"></div>
-    </main>
-  );
+class App extends Component() {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    },
+  };
+  render() {
+    const { store } = this.props;
+    return (
+      <main className="App">
+        <header className="App-header">
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className="App-list"></div>
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import STORE from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={STORE} />, document.getElementById('root'));
