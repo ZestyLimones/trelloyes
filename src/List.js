812856f1808d59d815afce997e3d430062bbf49e
Micheal Limones <micheal@zestylimones.com>
2021-10-26 15:18:15 -0600

    changed list js file into a function.

--- src/List.js
+++ src/List.js
@@ -2,21 +2,22 @@ import React from 'react';
 import Card from './Card';
 import './List.css';
 
-class List extends React.Component {
-  render() {
-    return (
-      <section className="List">
-        <header>this.props.header</header>
-        <div className="List-cards">
-          this.props.cards.map(function(card)
-          {<Card key={Card.id} title={Card.title} content={Card.content} />})
-          <button type="button" className="List-add-button">
-            Add Random Card
-          </button>
-        </div>
-      </section>
-    );
-  }
+function List(props) {
+  return (
+    <section className="List">
+      <header className="List-header">
+        <h2>{props.header}</h2>
+      </header>
+      <div className="List-cards">
+        {props.cards.map((card) => (
+          <Card key={card.id} title={card.title} content={card.content} />
+        ))}
+        <button type="button" className="List-add-button">
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default List;
